a1d75dc5a99dea24843a089b65c62feebdc8c700
MirboboyevJasurjan <MirboboyevJasurjan@gmail.com>
2022-08-01 07:12:25 +0500

    so First

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import First from './components/Fisrt/First';
+import Second from './components/Second/Second';
 
 function App() {
   return (
     <div className="App">
     
-    <First/>
+    {/* <First/> */}
+    <Second />
 
     </div>
   );
--- src/components/Fisrt/First.jsx
+++ src/components/Fisrt/First.jsx
@@ -5,18 +5,17 @@ import './First.css'
 function First() {
   return (
     <div className='firstMain'>
-        <Navbar/>
-        <div className='CreativeBlock'>
-            <div className='smmCreative'>SMM <br/>CREATIVE</div>
-            <hr className='line'/>
-            <div className='textCreative'>
-                <div className='smmText'>Shunchaki SMM’ni bilish kamlik qiladi,<br/>
-“SMM Creative” kursi - boshqacha yondashuv</div>
-                <div className=' button'>Kursga yozilish</div>
-            </div>
-
+      <Navbar />
+      <div className='CreativeBlock'>
+        <div className='smmCreative'>SMM <br />CREATIVE</div>
+        <hr className='line' />
+        <div className='textCreative'>
+          <div className='smmText'>Shunchaki SMM’ni bilish kamlik qiladi,<br />
+            “SMM Creative” kursi - boshqacha yondashuv</div>
+          <div className=' button'>Kursga yozilish</div>
         </div>
-      
+      </div>
+
     </div>
   )
 }
