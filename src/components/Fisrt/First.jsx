1fc86b19bfbae0dba6d1b1c211c57a529a52309d
StiLKnoT <stilknot@gmail.com>
2022-08-03 23:42:58 +0500

    dsdasapngblock

--- src/components/Fisrt/First.jsx
+++ src/components/Fisrt/First.jsx
@@ -28,6 +28,7 @@ import Word from '../../assets/images/Word.png'
 import Excell from '../../assets/images/Excell.png'
 import PowerP from '../../assets/images/PowerP.png'
 import Video from '../../assets/images/Video.png'
+import linesMob from '../../assets/icons/arrowBlockMob.png';
 
 
 function First() {
@@ -71,9 +72,10 @@ function First() {
       <div className=' button' id="buttonInvisebleMob"><a href='#tariflar'><ShadowButton /></a></div>
 
       {/* <Textline /> */}
-
-      <img className="firsLines" src={lines} />
-
+      <div className="pngBlock">
+        <img className="firsLines" src={lines} unselectable="on" />
+        <img className="firsLinesMob" src={linesMob} unselectable="on" />
+      </div>
 
       <ForWho />
       <ColorBlock />
@@ -88,7 +90,7 @@ function First() {
           <h2 className='ColorBlockH2'>O‘quvchilar uchun bonuslar</h2>
           <p className='ColorBlockP'>Biz o‘quvchilarga shunchaki dars beribgina qolmay, ular uchun foydali bo‘lgan bonuslarni ham taqdim etamiz!</p>
           <a onClick={()=>setBtnBonus(!btnBonus)}><div className='ShadowBtn'>
-            <p className='InBtn'>Bonuslarni ko‘rish <FaArrowRight /></p>
+            <p className='InBtn'>Bonuslarni ko‘rish<FaArrowRight /></p>
 
           </div>
           </a>
