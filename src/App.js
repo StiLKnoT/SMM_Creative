3dd3edbe43873b092e6b4482e559a150e04da05e
Mirboboyev Jasur <mirboboyevjasurjan@gmail.com>
2023-02-15 00:38:02 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
-import First from './components/Fisrt/First';
-import Second from './components/Second/Second';
-import Success from './components/atom/succes/Success';
+import Nav from "./components/template/navbar/Nav";
 
 
 function App() {
   return (
     <div className="App">
-      <Success/>
-    <First/>
-    <Second />
+      <Nav />
 
     </div>
   );
